6ce1f364e7cb6312ea0fb625728fe8dea1f36353
Patrik Lythell <patrik@ohmy.nu>
2017-10-14 20:21:13 +0200

    testing editing config, wonder what happens

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,28 @@
 <template>
   <div>
     <h1>{{content.title}}</h1>
+    <img :src="content.thumbnail" alt="">
+    <div class="test" v-html="content.__content"></div>
   </div>
 </template>
 
 <script>
-import content from '~/static/test.md';
+import content from '~/_posts/blog/2017-10-14-i-haven-t-seen-land-for-six-weeks.md';
 export default {
   data() {
     return {
       content
     }
+  },
+  mounted() {
+    console.log(this.content)
   }
 }
 </script>
 
 <style scoped>
+img {
+  max-width: 100%;
+}
 
 </style>
